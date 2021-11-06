f300b0210364c27dae916936512831d850a6ae88
Michael Fernando <michaelfernando@live.com>
2021-11-05 21:00:32 -0300

    SPA com React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Routes ,Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import MenuSuperior from "./components/MenuSuperior/MenuSuperior";
+import Resumo from "./components/Resumo/Resumo";
+import Consultas from "./components/Consulta/Consultas";
+import Faturamento from "./components/Faturamento/Faturamento";
+
+class App extends Component{
+  render(){
+    return(
+      <>
+    <MenuSuperior />
+
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+
+  
+        <Routes>
+        <Route exact path='/'  element={<Resumo />} />
+        <Route path='consultas/*' element={<Consultas />} />
+        <Route path='faturamento/*' element={<Faturamento />} />
+        </Routes>
+  </div>
+      </div>
     </div>
-  );
+
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/include/bootstrap.js
@@ -0,0 +1,6 @@
+import './jquery'
+import './popper'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+ //Pq o bootstrap esta dentro do node_modules por isso uso o dist
